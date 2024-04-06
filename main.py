381dc63978e7813d53130caedc17f5725b9c3a2e
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-04-06 16:43:33 +0530

    feat: add CORSMiddleware

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.students import students
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods=["*"],
+    allow_origins=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
 app.include_router(students)
