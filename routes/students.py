d341c231fc0b8388231340f2aee2173d6c380d54
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-04-06 18:16:56 +0530

    feat: add PATCH /students/{id} endpoint

--- routes/students.py
+++ routes/students.py
@@ -22,7 +22,8 @@ def create_student(student: Student):
     
     
 @students.get("/students")
-def get_students(country: Optional[str] = Query(None), age: Optional[int] = Query(None)):
+def get_all_students(country: Optional[str] = Query(None), age: Optional[int] = Query(None)):
+    # construct find query as per country and age
     query = {}
     if country:
         query["address.country"] = country 
@@ -44,3 +45,30 @@ def get_student(id: str):
         return JSONResponse(content=student, status_code=200)
     else:
         return HTTPException(status_code=404, detail="Student not Found!")
+    
+
+@students.patch("/students/{id}")
+def update_student(id: str, name: Optional[str] = Query(None), age: Optional[int] = Query(None), city: Optional[str] = Query(None), country: Optional[str] = Query(None)):
+    if name is None and age is None and city is None and country is None:
+        return HTTPException(status_code=400, detail="At least one of the field needs to be updated")
+    
+    # construct query for user updation
+    update_data = {}
+    if name is not None:
+        update_data["name"] = name
+        
+    if age is not None:
+        update_data["age"] = age
+        
+    if city is not None or country is not None:
+        update_data["address"] = {}
+        if city is not None:
+            update_data["address"]["city"] = city
+        if country is not None:
+            update_data["address"]["country"] = country
+            
+    result = studentsCollection.update_one({"_id": ObjectId(id)}, {"$set": update_data})
+    if result.modified_count == 0:
+        return HTTPException(status_code=404, detail="Student not found!")
+    
+    return JSONResponse(content={}, status_code=204)
